c35bd5f37b1a9772c2429d4e450de4a5320330f9
NiTHiN6282 <nithindileep6282@gmail.com>
2022-07-09 22:04:08 +0530

    chatpage streambuilder added!

--- lib/chatpage.dart
+++ lib/chatpage.dart
@@ -1,6 +1,8 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 import 'datalist.dart';
+import 'main.dart';
 
 class ChatPage extends StatefulWidget {
   const ChatPage({Key? key}) : super(key: key);
@@ -14,63 +16,78 @@ class _ChatPageState extends State<ChatPage> {
   Widget build(BuildContext context) {
     return Container(
       color: Color(0xff0e171c),
-      child: ListView.builder(
-          physics: NeverScrollableScrollPhysics(),
-          shrinkWrap: true,
-          itemCount: chatList.length,
-          itemBuilder: (context, index) {
-            return Material(
-              color: Color(0xff0e171c),
-              child: InkWell(
-
-                onTap: (){},
-                child: Container(
-                  padding: EdgeInsets.all(10),
-                  decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(20)),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Container(
-                        child: Row(
-                          children: [
-                            CircleAvatar(
-                              radius: 27,
-                              backgroundImage: AssetImage(chatList[index]['profilepic']),
-                            ),
-                            SizedBox(
-                              width: 20,
-                            ),
-                            Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Text(
-                                  chatList[index]['name'],
-                                  style: TextStyle(fontSize: 16,
-                                  color: Color(0xffafb3b5)),
+      child: StreamBuilder<QuerySnapshot>(
+          stream: FirebaseFirestore.instance
+              .collection("user")
+              .where("userid", isNotEqualTo: userId)
+              .snapshots(),
+          builder: (context, snapshot) {
+            if (!snapshot.hasData) {
+              return CircularProgressIndicator();
+            } else if (snapshot.hasData && snapshot.data!.docs.isEmpty) {
+              return Text("No Chats");
+            } else {
+              return ListView.builder(
+                  physics: NeverScrollableScrollPhysics(),
+                  shrinkWrap: true,
+                  itemCount: snapshot.data!.docs.length,
+                  itemBuilder: (context, index) {
+                    return Material(
+                      color: Color(0xff0e171c),
+                      child: InkWell(
+                        onTap: () {},
+                        child: Container(
+                          padding: EdgeInsets.all(10),
+                          decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(20)),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Container(
+                                child: Row(
+                                  children: [
+                                    CircleAvatar(
+                                      radius: 27,
+                                      backgroundImage: NetworkImage(snapshot
+                                          .data!.docs[index]['userimage']),
+                                    ),
+                                    SizedBox(
+                                      width: 20,
+                                    ),
+                                    Column(
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.start,
+                                      children: [
+                                        Text(
+                                          snapshot.data!.docs[index]
+                                              ['username'],
+                                          style: TextStyle(
+                                              fontSize: 16,
+                                              color: Color(0xffafb3b5)),
+                                        ),
+                                        // SizedBox(
+                                        //   height: 12,
+                                        // ),
+                                        // Text(
+                                        //   snapshot.data!.docs[index]
+                                        //       ['username'],
+                                        //   style: TextStyle(
+                                        //       color: Color(0xff728088)),
+                                        // ),
+                                      ],
+                                    )
+                                  ],
                                 ),
-                                SizedBox(
-                                  height: 12,
-                                ),
-                                Text(chatList[index]['chatshort'],
-                                style: TextStyle(
-                                    color: Color(0xff728088)
-                                ),),
-                              ],
-                            )
-                          ],
+                              ),
+                              // Text(chatList[index]['time'],
+                              //     style: TextStyle(color: Color(0xff728088)))
+                            ],
+                          ),
                         ),
                       ),
-                      
-                      Text(chatList[index]['time'],
-                          style: TextStyle(
-                              color: Color(0xff728088)
-                          ))
-                    ],
-                  ),
-                ),
-              ),
-            );
+                    );
+                  });
+            }
           }),
     );
   }
