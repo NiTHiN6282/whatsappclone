c5772964e85cc3ccfae23add0883142efe9a6b22
NiTHiN6282 <nithindileep6282@gmail.com>
2022-07-25 11:36:18 +0530

    cached network image added to message page!

--- lib/chat/messagepage.dart
+++ lib/chat/messagepage.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 import 'dart:io';
 
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:emoji_picker_flutter/emoji_picker_flutter.dart';
 import 'package:flutter/material.dart';
@@ -74,7 +75,7 @@ class _MessagePageState extends State<MessagePage> {
                   },
                   icon: Icon(Icons.arrow_back_ios_new)),
               CircleAvatar(
-                backgroundImage: NetworkImage(widget.profilePic),
+                backgroundImage: CachedNetworkImageProvider(widget.profilePic),
               ),
             ],
           ),
